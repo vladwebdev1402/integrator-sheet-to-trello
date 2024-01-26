cfe433dd807129ff54116ae1a69e9667b7c83217
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-01-27 00:02:31 +0700

    fix skeleton avatars

--- src/entities/board-card-members/ui/BoardCardMembers.tsx
+++ src/entities/board-card-members/ui/BoardCardMembers.tsx
@@ -29,9 +29,9 @@ const BoardCardMembers = () => {
           </AvatarGroup>
           {isLoading && (
             <Box display="flex" alignItems="center" gap="3px">
-              <Skeleton width="24px" height="24px" variant="circular" />,
-              <Skeleton width="24px" height="24px" variant="circular" />,
-              <Skeleton width="24px" height="24px" variant="circular" />,
+              <Skeleton width="24px" height="24px" variant="circular" />
+              <Skeleton width="24px" height="24px" variant="circular" />
+              <Skeleton width="24px" height="24px" variant="circular" />
             </Box>
           )}
         </Box>
