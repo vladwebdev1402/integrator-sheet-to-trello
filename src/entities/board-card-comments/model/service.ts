5c899bcfec006c7d578f46cdd57be7a7a9628215
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-01-27 13:22:04 +0700

    fix: filter comments

--- src/entities/board-card-comments/model/service.ts
+++ src/entities/board-card-comments/model/service.ts
@@ -11,7 +11,7 @@ export const TrelloCardCommentsService = createApi({
             query: (id) => ({
                 url: `/cards/${id}/actions`,
                 params: {
-                    type: "commentCard",
+                    filter: "commentCard",
                 },
             })
         })    
