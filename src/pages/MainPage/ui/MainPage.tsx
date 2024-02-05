022e193c3a0708773a845987c7c4955878f48764
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-02-05 18:55:28 +0700

    check auth in main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,18 +1,53 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Box, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 import { useGoogleAuth, useTrelloAuth } from "@/features/auth";
 import { Converter } from "@/widgets/converter";
+import { useAppSelector } from "@/shared/hooks";
+import { routerPaths } from "@/shared/constants";
 
 const MainPage = () => {
+  const navigate = useNavigate();
+
+  const authorizeClick = () => {
+    navigate(routerPaths.profile);
+  };
+  const { isAuth: isTrelloAuth } = useAppSelector(
+    (state) => state.AuthTrelloReducer
+  );
+  const { isAuth: isGoogleAuth } = useAppSelector(
+    (state) => state.AuthGoogleReducer
+  );
+
   useGoogleAuth();
   useTrelloAuth();
 
   return (
     <Container className={`container`}>
-      <Typography textAlign={"center"} variant="h3" marginTop="48px">
-        Converter
-      </Typography>
-      <Converter />
+      {isTrelloAuth && isGoogleAuth && (
+        <>
+          <Typography textAlign={"center"} variant="h3" marginTop="48px">
+            Converter
+          </Typography>
+          <Converter />
+        </>
+      )}
+      {!isTrelloAuth || !isGoogleAuth ? (
+        <Box textAlign="center">
+          <Typography textAlign={"center"} variant="h5" marginTop="48px">
+            You are not logged in to Google or Trello account
+          </Typography>
+          <Button
+            onClick={authorizeClick}
+            sx={{ margin: "25px" }}
+            variant="contained"
+          >
+            Authorize
+          </Button>
+        </Box>
+      ) : (
+        <></>
+      )}
     </Container>
   );
 };
